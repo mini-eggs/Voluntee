78db4615f04bca1143d4e30e48a76d6f44cffb20
Evan <evan@crowdhubapps.com>
2016-12-10 14:05:55 -0800

    code push is active

--- client/Voluntee/index.ios.js
+++ client/Voluntee/index.ios.js
@@ -1,5 +1,26 @@
 import React from 'react'
 import {AppRegistry} from 'react-native'
+import codePush from 'react-native-code-push'
 import Voluntee from './src/'
 
-AppRegistry.registerComponent('Voluntee', props => Voluntee)
+const codePushOptions = {checkFrequency: codePush.CheckFrequency.ON_APP_RESUME}
+
+class CodePush extends React.Component {
+
+	async componentDidMount() {
+		codePush.sync({
+            updateDialog: true,
+            installMode: codePush.InstallMode.IMMEDIATE
+        })
+	}
+
+	render() {
+		return (
+			<Voluntee/>
+		)
+	}
+}
+
+const appBundle = codePush(codePushOptions)(CodePush)
+
+AppRegistry.registerComponent('Voluntee', props => appBundle)
