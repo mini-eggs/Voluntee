7004bf43e40bc45d8063c616b8c27b4bfb6581b7
Evan <evan@crowdhubapps.com>
2017-01-19 16:27:27 -0800

    you can now use the app fine with locations disabled

--- client/Voluntee/src/comps/items/items.js
+++ client/Voluntee/src/comps/items/items.js
@@ -15,146 +15,175 @@ import {noInternetConnection} from '../../general/userActions'
 import EventListComp from './list'
 
 const style = {
-    Text:{
-        textAlign:'center',
-        fontSize:18,
-        color:'#fff',
-        fontWeight:'600'
-    },
-    TextLeft:{
-        fontSize:18
-    },
-    TextInput:{
-        // height:51.5,
-        height:53,
-        textAlign:'center',
-        fontSize:18,
-        color:'#fff',
-        fontWeight:'600'
-    }
+  Text: {
+    textAlign: 'center',
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: '600'
+  },
+  TextLeft: {
+    fontSize: 18
+  },
+  TextInput: {
+    // height:51.5,
+    height: 53,
+    textAlign: 'center',
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: '600'
+  }
 }
 
 class Items extends React.Component {
 
-    constructor(props){
-        // set up extended comp
-        super(props)
-        // register left map button
-        // to use this component
-        Actions.ViewEventsOnMap = () => { 
-            // view all current events
-            // in map component
-            displayListOnMap({events: this.state.events})
-        }
-        // get initial state
-        this.state = {
-            loading:true,
-            loadingNextPage:false,
-            morePages:true,
-            zip:null,
-            userInputZip:null,
-            page:0,
-            events:[],
-            userWantsToChangeZip:false,
-            noInternet: false
-        }
-    }
-
-    componentDidMount(){
-      this.initializeSearch()
-    }
-
-    async initializeSearch() {
-        try {
-            const loc = await getLocation()
-            const zip = await getZipFromLatAndLong({lat:loc.coords.latitude,long:loc.coords.longitude})
-            this.componentWillReceiveZip(zip.postalCodes[0].postalCode)
-        }
-        catch(err) {
-            if(__DEV__) {
-                console.log('Error in componentDidMount within items.js, error below: ')
-                console.log(err)
-            }
-            if(err.message === 'Location request timed out') {
-              Actions.modal({
-                header: 'Error',
-                message: 'Could not find current zip. Please make sure locations are enabled.',
-                onComplete: () => { this.setState({zip:'00000', loading: false, morePages:false, userWantsToChangeZip: true}) }
-              })
-            } 
-            else {
-              await noInternetConnection()
-              this.setState({ loading: false, noInternet: true })
-            }
-        }
-    }
-
-    componentWillReceiveZip(zip){
-        this.setState({zip:zip},this.componentWillSearchOpportunities)
-    }
-
-    async componentWillSearchOpportunities(){
-        let data = {zip:this.state.zip,page:this.state.page}
-        searchOpportunities(data)
-            .then( events => {
-                let newEventsArr = [...this.state.events, ...events]
-                let morePages = (events.length >= 10)
-                this.setState({events:newEventsArr,loading:false,loadingNextPage:false, morePages:morePages})
-            })
-            .catch( err => console.log(err))
-    }
-
-    componentWillLoadNextPage(){
-      const newState = {page:(this.state.page + 1),loadingNextPage:true}
-      this.setState(newState, this.componentWillSearchOpportunities)
-    }
-
-    componentWillChangeZip(zip){
-        if(!zip) {
-          return false
-        }
-        else if(zip.toString().length === 5 && parseInt(zip).toString() === zip.toString()) {
-            const data = {
-                userInputZip:'',
-                zip:zip,
-                page:0,
-                loading:true,
-                events:[],
-                userWantsToChangeZip:false
-            }
-            this.setState(data, this.componentWillSearchOpportunities)
-        }
-        else {
-            Actions.modal({
-                header:'Error',
-                message:'Zip has been entered incorrectly'
-            })
-        }
-    }
-
-    componentWillDetermineSearch() {
-        const fail = e => this.setState({userWantsToChangeZip:false})
-        if(!this.state.userInputZip) {
-          fail()
-        }
-        else if(!(this.state.userInputZip.length == 5)) {
-          fail()
-        }
-        else {
-          this.componentWillChangeZip(this.state.userInputZip)
-        }
-    }
-
-    retryWithNoInternet() {
-      this.setState({ loading: true, noInternet: false }, () => {
-        this.initializeSearch()
+  constructor(props) {
+    // set up extended comp
+    super(props)
+    // register left map button
+    // to use this component
+    Actions.ViewEventsOnMap = () => {
+      // view all current events
+      // in map component
+      displayListOnMap({
+        events: this.state.events
       })
     }
+    // get initial state
+    this.state = {
+      loading: true,
+      loadingNextPage: false,
+      morePages: true,
+      zip: null,
+      userInputZip: null,
+      page: 0,
+      events: [],
+      userWantsToChangeZip: false,
+      noInternet: false
+    }
+  }
 
-  	render() {
-    	return (
-    		<Base>
-                {
+  componentDidMount() {
+    this.initializeSearch()
+  }
+
+  async initializeSearch() {
+    getLocation()
+      .then( loc => {
+        getZipFromLatAndLong({ lat: loc.coords.latitude, long: loc.coords.longitude })
+          .then( zip => {
+            this.componentWillReceiveZip(zip.postalCodes[0].postalCode)
+          })
+          .catch( err => {
+            if(__DEV__) {
+              console.log('Error in componentDidMount within items.js, error below: ')
+              console.log(err)
+            }
+            noInternetConnection()
+              .then( () => {
+                this.setState({ loading: false, noInternet: true })
+              })
+          })
+
+      })
+      .catch( err => {
+        if(__DEV__) {
+          console.log('Error in componentDidMount within items.js, error below: ')
+          console.log(err)
+        }
+        Actions.modal({
+          header: 'Woah!',
+          message: 'Could not retreive current zip. Make sure locations are enabled.',
+          onComplete: () => {
+            this.setState({ zip: '00000', loading: false, morePages: false, userWantsToChangeZip: true })
+          }
+        })
+      })
+  }
+
+  componentWillReceiveZip(zip) {
+    this.setState({
+      zip: zip
+    }, this.componentWillSearchOpportunities)
+  }
+
+  async componentWillSearchOpportunities() {
+    let data = {
+      zip: this.state.zip,
+      page: this.state.page
+    }
+    searchOpportunities(data)
+      .then(events => {
+        let newEventsArr = [...this.state.events, ...events]
+        let morePages = (events.length >= 10)
+        this.setState({
+          events: newEventsArr,
+          loading: false,
+          loadingNextPage: false,
+          morePages: morePages
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  componentWillLoadNextPage() {
+    const newState = {
+      page: (this.state.page + 1),
+      loadingNextPage: true
+    }
+    this.setState(newState, this.componentWillSearchOpportunities)
+  }
+
+  componentWillChangeZip(zip) {
+    if(!zip) {
+      return false
+    }
+    else if(zip.toString().length === 5 && parseInt(zip).toString() === zip.toString()) {
+      const data = {
+        userInputZip: '',
+        zip: zip,
+        page: 0,
+        loading: true,
+        events: [],
+        userWantsToChangeZip: false
+      }
+      this.setState(data, this.componentWillSearchOpportunities)
+    }
+    else {
+      Actions.modal({
+        header: 'Error',
+        message: 'Zip has been entered incorrectly'
+      })
+    }
+  }
+
+  componentWillDetermineSearch() {
+    const fail = e => this.setState({
+      userWantsToChangeZip: false
+    })
+    if(!this.state.userInputZip) {
+      fail()
+    }
+    else if(!(this.state.userInputZip.length == 5)) {
+      fail()
+    }
+    else {
+      this.componentWillChangeZip(this.state.userInputZip)
+    }
+  }
+
+  retryWithNoInternet() {
+    this.setState({
+      loading: true,
+      noInternet: false
+    }, () => {
+      this.initializeSearch()
+    })
+  }
+
+  render() {
+    return(
+      <Base>
+        {
                     this.state.loading || this.state.noInternet ?
                         <View>
                           {
@@ -206,9 +235,9 @@ class Items extends React.Component {
                             {!this.state.loadingNextPage && this.state.morePages ? <Button raised primary text="MORE" onPress={ e => { this.componentWillLoadNextPage() }} /> : <View/>}
                         </View>
                 }
-    		</Base>
-    	)
-  	}
+        </Base>
+    )
+  }
 }
 
 export default Items
